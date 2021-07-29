83412dac13b8deaac0d6e639eec998d3a380b0f1
GuilhermeDellatin <guidellatin@gmail.com>
2021-07-28 22:31:38 -0300

    Create menu and Search resources

--- app/src/main/java/com/gfdellatin/meuportfolio/ui/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/meuportfolio/ui/MainActivity.kt
@@ -2,11 +2,41 @@ package com.gfdellatin.meuportfolio.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.gfdellatin.meuportfolio.R
+import com.gfdellatin.meuportfolio.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
     }
 }
