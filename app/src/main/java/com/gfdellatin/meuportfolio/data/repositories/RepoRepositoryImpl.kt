7c0744c8dbec5a0dbc9ebc2e035bac2989607055
GuilhermeDellatin <guidellatin@gmail.com>
2021-07-31 13:07:02 -0300

    Create personalized exception and adjusting variables

--- app/src/main/java/com/gfdellatin/meuportfolio/data/repositories/RepoRepositoryImpl.kt
+++ app/src/main/java/com/gfdellatin/meuportfolio/data/repositories/RepoRepositoryImpl.kt
@@ -1,12 +1,18 @@
 package com.gfdellatin.meuportfolio.data.repositories
 
+import com.gfdellatin.meuportfolio.core.RemoteException
 import com.gfdellatin.meuportfolio.data.services.GitHubService
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(private val service: GitHubService) : RepoRepository {
 
     override suspend fun listRepositories(user: String) = flow {
-        val repoList = service.listRepositories(user)
-        emit(repoList)
+        try {
+            val repoList = service.listRepositories(user)
+            emit(repoList)
+        } catch (ex: HttpException) {
+            throw RemoteException(ex.message ?: "Não foi possível fazer a busca no momento!")
+        }
     }
 }
