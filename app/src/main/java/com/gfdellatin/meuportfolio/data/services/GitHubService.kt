e33b6d1cb81428581a37dfb58a7e97e4347a2e93
guilherme.dellatin <Fiorilli@2020>
2021-08-10 15:30:49 -0300

    Adjustament in UseCase and using suspend fun in GithubService

--- app/src/main/java/com/gfdellatin/meuportfolio/data/services/GitHubService.kt
+++ app/src/main/java/com/gfdellatin/meuportfolio/data/services/GitHubService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.Path
 
 interface GitHubService {
     @GET("users/{user}/repos")
-    fun  listRepositories(@Path("user") user: String) : List<Repo>
+    suspend fun  listRepositories(@Path("user") user: String) : List<Repo>
 }
